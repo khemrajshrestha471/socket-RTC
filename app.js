73ad686035ec5b2fa07da01a3a1d5c680ec492e2
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-09-27 07:59:19 +0545

    socket with real time communication with server started

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js');
+const path = require('path');
+const { log } = require('console');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render('index', { title: "Chess Game" });
+})
+
+io.on("connection", function (uniquesocket) {
+    console.log("connected");
+
+    if(!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    }
+    else if(!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    uniquesocket.on("disconnect", function () {
+        if(uniquesocket.id === players.white) {
+            delete players.white;
+        } else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    })
+})
+ 
+server.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
