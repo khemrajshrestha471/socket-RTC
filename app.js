faffffbdb3d80d2b480abde7774668f89827b62e
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-09-27 08:25:26 +0545

    logic implemented for chess using fen equation

--- app.js
+++ app.js
@@ -42,6 +42,27 @@ io.on("connection", function (uniquesocket) {
             delete players.black;
         }
     })
+
+    uniquesocket.on("move", (move) => {
+        try {
+            if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+            if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if(result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen())
+            } else {
+                console.log("Invalid move: ", move);
+                uniquesocket.emit("invalidMove ", move);
+            }
+
+        } catch (error) {
+            console.log(error);
+            uniquesocket.emit("Invalid move: ", move);
+        }
+    })
 })
  
 server.listen(3000, function(){
