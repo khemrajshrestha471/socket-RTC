4673573311577fd40cb9de3cafff69b2b2f9e07e
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-09-27 11:13:44 +0545

    chess game completed with socket and rtc

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -21,7 +21,7 @@ const renderBoard = () => {
             if(square) {
                 const pieceElement = document.createElement("div");
                 pieceElement.classList.add("piece", square.color === 'w' ? "white" : "black")
-                pieceElement.innerHTML = "";
+                pieceElement.innerHTML = getPieceUnicode(square);
                 pieceElement.draggable = playerRole === square.color;
 
                 pieceElement.addEventListener("dragstart", (e) => {
@@ -55,11 +55,58 @@ const renderBoard = () => {
             boardElement.appendChild(squareElement);
         })
     })
-
+    if(playerRole === "b") {
+        boardElement.classList.add("flipped");
+    } else {
+        boardElement.classList.remove("flipped");
+    }
 }
 
-const handleMove = () => {}
+const handleMove = (source, target) => {
+    const move = {
+        from: `${String.fromCharCode(97+source.col)}${8 - source.row}`,
+        to: `${String.fromCharCode(97+target.col)}${8 - target.row}`,
+        promotion: 'q',
+    }
+    socket.emit('move', move);
+}
 
-const getPieceUnicode = () => {}
+const getPieceUnicode = (piece) => {
+    const unicodePieces = {
+        p: "♙",
+        r: "♖",
+        n: "♘",
+        b: "♗",
+        q: "♕",
+        k: "♔",
+        P: "♟",
+        R: "♜",
+        N: "♞",
+        B: "♝",
+        Q: "♛",
+        K: "♚",
+    }
+    return unicodePieces[piece.type] || "";
+}
+
+socket.on("playerRole", function (role) {
+    playerRole = role;
+    renderBoard();
+})
+
+socket.on("spectatorRole", function () {
+    playerRole = null;
+    renderBoard();
+})
+
+socket.on("boardState", function (fen) {
+    chess.load(fen);
+    renderBoard();
+})
+
+socket.on("move", function (move) {
+    chess.move(move);
+    renderBoard();
+})
 
 renderBoard();
